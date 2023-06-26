72b4dab78f4e127cf1240e80e86dc760ad419e09
dancan-agweli <agwelidancan32@gmail.com>
2023-06-26 12:14:57 -0700

    sorted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,52 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap doubly-linked list.
- * @h: A pointeer to head of linked list
- * @n1: first node to swap.
- * @n2: The second node to swap.
+ * swap - swaps integers  in a doubly-linked list
+ * @a: first integer
+ * @b: second integer
+ * Return: swapped numbers
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap(listint_t *a, listint_t *b)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list
- * @list: A pointer to the head of a doubly-linked 
- * Description: output the list of linked and sorted
+ * insertion_sort_list - doubly-linked list
+ * @list: pointer to head node
+ * Return: answer always
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *y, *so, *buff;
+	listint_t *x, *y;
 
-	if (!list || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
-
-	for (y = (*list)->next; y != NULL; y = buff)
+	x = (*list)->next;
+	if (x)
 	{
-		buff = y->next;
-		so = y->prev;
-		if (so != NULL && y->n < so->n)
+		y = x;
+		x = x->next;
+		while (y && y->prev)
 		{
-			swap_nodes(list, &so, y);
-			print_list((const listint_t *)*list);
+			if (y->prev->n > y->n)
+			{
+				swap(y->prev, y);
+				if (!y->prev)
+					*list = y;
+				print_list((const listint_t *)*list);
+			}
+			else
+				y = y->prev;
 		}
+
 	}
 }
