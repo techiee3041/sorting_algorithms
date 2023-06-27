3dfa0d5f0e3e5c1197ba2fc3bb4c11cbc8d807c7
dancan-agweli <agwelidancan32@gmail.com>
2023-06-27 12:29:23 -0700

    sorted

--- sort.h
+++ sort.h
@@ -37,5 +37,6 @@ void counting_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
+void merge_recursion(int *arr, int *array, size_t left, size_t right);
+void merge_subarray(int *arr, int *array, size_t left,size_t mid, size_t right);
 #endif
