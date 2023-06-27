2e077e6c1cb3c18237155bac1da09ee156f57456
dancan-agweli <agwelidancan32@gmail.com>
2023-06-27 12:15:38 -0700

    sorted array

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,33 +9,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-    int x = 0, y = 0, s = 1, a = 0;
+	int x = 0, y = 0, s = 1, a = 0;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    while (s < (int)size / 3)
-        s = s * 3 + 1;
-
-    while (s > 0)
-    {
-        y = s;
-        while (y < (int)size)
-        {
-            a = array[y];
-            x = y;
-
-            while (x > s - 1 && array[x - s] >= a)
-            {
-                array[x] = array[x - s];
-                x = x - s;
-            }
-
-            array[x] = a;
-            y++;
-        }
-        print_array(array, size);
-        s = (s - 1) / 3;
-    }
+	while (s < (int)size / 3)
+		s = s * 3 + 1;
+	while (s > 0)
+	{
+		y = s;
+		while (y < (int)size)
+		{
+			a = array[y];
+			x = y;
+			while (x > s - 1 && array[x - s] >= a)
+			{
+				array[x] = array[x - s];
+				x = x - s;
+			}
+		array[x] = a;
+		y++;
+	}
+	print_array(array, size);
+	s = (s - 1) / 3;
+	}
 }
 
